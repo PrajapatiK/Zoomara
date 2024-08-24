0822389460e2c44d532c4802d6243d11fb4e5001
Shailendra Kumar <shailendra.kumar@svayamtech.com>
2024-08-24 13:25:09 +0530

    Update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { Toaster } from "@/components/ui/toaster"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "YOOM",
+  title: "Zoomara",
   description: "Video calling App",
   icons: {
     icon: "/icons/logo.svg",
